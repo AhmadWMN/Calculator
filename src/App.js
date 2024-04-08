04c676cb63c43bd9ac71859b79783e4df3bcef72
AhmadWN <ahmadwnabhan2023@gmail.com>
2024-04-08 08:09:39 +0300

    rewrite code to make it readablilty and fix some logical errors

--- src/App.js
+++ src/App.js
@@ -5,94 +5,158 @@ import './App.css';
 
 function App() {
 
-  // let operand1 = 0;
-  // let operand2 = 0;
-  // let result= 0;
+  const [show , setShow] = useState('');
+  const [isFirstOperand,setIsFirstOperand] = useState(true);
+  const [operand1,setOperand1] = useState('');
+  const [operand2,setOperand2] = useState('');
+  const [operation,setOperation] = useState('');
+  const [result, setResult] = useState('');
 
 
-  // const [result , setResult] = useState(0);
-  const [operand1, setOperand1] = useState(0);
-  // const [operand2, setOperand2] = useState(0);
-  const [result , setResult] = useState(0);
-  const [chooseOperation , setChooseOperation] = useState(false);
 
+  const setInformation =(info)=>  {
 
-  // const setOperand1 =(number)=>  {
+    const operations = ['+','-','x','/'];
 
-  //   operand1 = operand1+""+number;
-  //   console.log(operand1)
+    if(operations.includes(info)) {
 
-  // }
+      setIsFirstOperand(false);
+      setOperation(info);    
 
-  // const setOperand2 =(number)=> {
+    } else {
 
-  //   operand2 = number;
-  //   console.log(operand2);
     
 
-  // }
+    if(isFirstOperand) {
 
-  // const setResult =()=> {
+      setOperand1(operand1+""+info);
 
-  // }
+    } else {
+
+      setOperand2(operand2+""+info);
+
+    }
 
 
-  const divideOperation =()=> {
+    if(info ==='='){
+
+      setIsFirstOperand(true);
+
+      switch (operation){
+
+        case '+':
+
+          setResult(addOperation(operand1,operand2));
+          break;
+
+        case '-':
+
+          setResult(subtractOperation(operand1,operand2));
+          break;
+   
+        case 'x':
+
+          setResult(multiplyOperation(operand1,operand2));
+          break;
+
+        case '/':  
+
+          setResult(divideOperation(operand1,operand2));
+          break;
+          
+      }   
+    }
 
   }
 
-  const multiplyOperation =()=> {
+  }
+
+  const divideOperation =(operand1 ,operand2)=> {
+
+    let result;
+    result= Number(operand1) / Number(operand2);
+    return result ;
 
   }
 
-  const subtractOperation =()=> {
+  const multiplyOperation =(operand1 ,operand2)=> {
+
+    let result;
+    result = Number(operand1) * Number(operand2); 
+    return result;
 
   }
 
-  const addOperation =()=> {
+  const subtractOperation =(operand1 ,operand2)=> {
 
+    let result;
+    result = Number(operand1) - Number(operand2);
+    return result;
+
+  }
+
+  const addOperation =(operand1 ,operand2)=> {
+
+    let result;
+    result =Number(operand1) + Number(operand2);
+    return result;
+
+  }
+
+  const clearScreen = () => {
+    setOperand1('');
+    setOperand2('');
+    setOperation('');
+    setResult('');
   }
 
 
   useEffect(()=>{
 
-    setResult(operand1);
+    setShow(operand1+" "+operation+" "+operand2+" "+result);
 
-  },[operand1])
+
+    console.log(operand2);
+    console.log(result);
+
+  },[operand1,operand2,operation]);
+
+
+
 
   return (
     <div class="top-container">
     
     <div class="result-container">
 
-      <div class="btn-clear">
-        <button>C</button>
+      <div class="btn-clear" onClick={()=> {clearScreen()}}>
+        C
       </div> 
 
-
       <div class="result-field">
-        <h2>{result}</h2>
+        <h2>{show}</h2>
       </div>
 
     </div>
 
     <div class="grid-container">
-      <div class="grid-item" onClick={()=>{setOperand1(1)}}>1</div>
-      <div class="grid-item" onClick={()=>{setOperand1(2)}}>2</div>
-      <div class="grid-item" onClick={()=>{setOperand1(3)}}>3</div>  
-      <div class="grid-item" onClick={()=>{setOperand1(1)}}>/</div> 
-      <div class="grid-item" onClick={()=>{setOperand1(4)}}>4</div>
-      <div class="grid-item" onClick={()=>{setOperand1(5)}}>5</div>
-      <div class="grid-item" onClick={()=>{setOperand1(6)}}>6</div>  
-      <div class="grid-item" onClick={()=>{setOperand1(1)}}>*</div>
-      <div class="grid-item" onClick={()=>{setOperand1(7)}}>7</div>
-      <div class="grid-item" onClick={()=>{setOperand1(8)}}>8</div> 
-      <div class="grid-item" onClick={()=>{setOperand1(9)}}>9</div>
-      <div class="grid-item" onClick={()=>{setOperand1(1)}}>-</div>
-      <div class="grid-item" onClick={()=>{setOperand1(0)}}>0</div> 
-      <div class="grid-item" onClick={()=>{setOperand1(1)}}>.</div>
-      <div class="grid-item" onClick={()=>{setOperand1(1)}}>=</div>
-      <div class="grid-item" onClick={()=>{setOperand1(1)}}>+</div>  
+      <div class="grid-item upper-left-corner" onClick={()=>{setInformation('1')}}>1</div>
+      <div class="grid-item" onClick={()=>{setInformation('2')}}>2</div>
+      <div class="grid-item" onClick={()=>{setInformation('3')}}>3</div>  
+      <div class="grid-item upper-right-corner" onClick={()=>{setInformation('/')}}>/</div> 
+      <div class="grid-item" onClick={()=>{setInformation('4')}}>4</div>
+      <div class="grid-item" onClick={()=>{setInformation('5')}}>5</div>
+      <div class="grid-item" onClick={()=>{setInformation('6')}}>6</div>  
+      <div class="grid-item" onClick={()=>{setInformation('x')}}>*</div>
+      <div class="grid-item" onClick={()=>{setInformation('7')}}>7</div>
+      <div class="grid-item" onClick={()=>{setInformation('8')}}>8</div> 
+      <div class="grid-item" onClick={()=>{setInformation('9')}}>9</div>
+      <div class="grid-item" onClick={()=>{setInformation('-')}}>-</div>
+      <div class="grid-item lower-left-corner" onClick={()=>{setInformation('0')}}>0</div> 
+      <div class="grid-item" onClick={()=>{setInformation('.')}}>.</div>
+      <div class="grid-item" onClick={()=>{setInformation('=')}}>=</div>
+      <div class="grid-item lower-right-corner" onClick={()=>{setInformation('+')}}>+</div> 
+       
     </div>
 
     </div>
